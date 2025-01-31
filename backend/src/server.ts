b00d2174d8e310f47441c8a6094c547711e01ccb
Govind Gupta <govind12771277@gmail.com>
2025-01-31 15:17:56 +0530

    backend: setup complete

---
+++ backend/src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { PrismaClient } from '@prisma/client';
+import authRoute from "./routes/auth.route.js"
+const prisma = new PrismaClient();
+
+dotenv.config({
+  path: './.env',
+});
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use("/v1/auth", authRoute);
+app.listen(PORT, async () => {
+  try {
+    console.log(`Server running on port ${PORT}`);
+    await prisma.$connect();
+    console.log('Dababase connected');
+  } catch (error) {
+    console.log('Internal server error: ', error);
+  }
+});
