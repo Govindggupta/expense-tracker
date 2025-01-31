2a5f940604e751717743aef29b4096b8e1b25c95
Govind Gupta <govind12771277@gmail.com>
2025-01-31 16:46:47 +0530

    App: setup complete

---
+++ frontend/mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="bg-slate-500">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
